7544e79087c2d6ea1dc69a3e5083d5b3655c993f
Diego Garay <diegogaraycullas@gmail.com>
2023-01-19 15:49:09 -0500

    optimizando para render

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,26 +4,35 @@ require('dotenv').config();
 const authMiddleware = (req, res, next) => {
   //para obtener el authorization puedo hacerlo asi
   let { authorization: token } = req.headers;
-  token = token.replace('Bearer ', '');
+  token = token?.replace('Bearer ', '');
+
+
   //o tambien asi
   //const token = req.headers.authorization;
   console.log(token);
-  jwt.verify(
-    token, 
-    process.env.JWT_SECRET,  //aqui iba la palabra secreta pero va a ir los env
-    { algorithms: 'HS512' },
-    (err, decoded) => {
-      if (err) {
-        res.status(400).json({
-          error: 'invalid token',
-          message: 'El token no es válido o ya expiro, envía un token correcto'
-        });
-      } else {
-        console.log(decoded);
-        next();
+  if (token) {
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET,  //aqui iba la palabra secreta pero va a ir los env
+      { algorithms: 'HS512' },
+      (err, decoded) => {
+        if (err) {
+          res.status(400).json({
+            error: 'invalid token',
+            message: 'El token no es válido o ya expiro, envía un token correcto'
+          });
+        } else {
+          console.log(decoded);
+          next();
+        }
       }
-    }
-  );
+    );
+  } else {
+    res.status(400).json({
+      error: "no token provided",
+      message: "No se esta enviando un token de autenticacion"
+    })
+  }
 
 }
 
